9f25df392ca132598be18f0ae529b33b2a1c1f67
yuyaamano23 <nba.chako.nba@icloud.com>
2021-02-12 19:07:17 +0900

    環境構築huskyやる手前まで

---
+++ src/pages/_app.tsx
@@ -0,0 +1,9 @@
+import 'styles/globals.scss'
+import React from 'react'
+import { AppProps } from 'next/app'
+
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
