73d5457b2352e3b8d7a8d17a8464a296e105ff92
yuyaamano23 <nba.chako.nba@icloud.com>
2021-02-14 02:21:13 +0900

    削除でけた！

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,22 +1,44 @@
 import React from 'react'
 import { Todo } from 'components/Types'
 import Link from 'next/link'
+import axios from 'axios'
 
 type TodoItemProps = {
   todo: Todo
 }
 
 const TodoItem: React.FC<TodoItemProps> = ({ todo }) => {
+  const handleDelete = (e) => {
+    // ボタンのデフォルトの挙動を葬り去る
+    e.preventDefault()
+
+    axios({
+      method: 'delete',
+      url: `http://localhost:8000/api/todos/${todo.id}`,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(() => {
+        console.log('delete success')
+        // ページ更新させる
+        // Router.push('/todos')
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
   return (
     <>
-      <h3>------------------------------------------</h3>
-      <h1>id:{todo.id}</h1>
-      <h1>内容:{todo.title}</h1>
-      <button>削除</button>
+      <h5>------------------------------------------</h5>
+      <h5>id:{todo.id}</h5>
+      <h5>内容:{todo.title}</h5>
+      <button onClick={handleDelete}>削除</button>
       <Link href={`/todos/${todo.id}`}>
         <a>記事詳細へ</a>
       </Link>
-      <h3>------------------------------------------</h3>
+      <h5>------------------------------------------</h5>
     </>
   )
 }
