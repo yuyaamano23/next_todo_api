0f45a438d76cb777c615c459267e0a7b062039ef
yuyaamano23 <nba.chako.nba@icloud.com>
2021-02-13 18:19:36 +0900

    記事詳細ページの動的ルーティングでけた！！

---
+++ src/pages/todos/[id].tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { Todo } from 'components/Types'
+import { GetStaticPaths, GetStaticProps } from 'next'
+
+type TodoItemProps = {
+  todo: Todo
+}
+
+const TodoItemDetail: React.FC<TodoItemProps> = ({ todo }) => {
+  return (
+    <>
+      <h1>ID: {todo.id}</h1>
+      <h1>TITLE: {todo.title}</h1>
+      <h1>内容 {todo.content}</h1>
+      <button>削除</button>
+    </>
+  )
+}
+
+export default TodoItemDetail
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  // 外部APIエンドポイントを呼び出しデータ取得
+  const res = await fetch('http://localhost:8000/api/todos')
+  const todos = await res.json()
+
+  // 事前ビルドしたいパスを指定
+  const paths = todos.map((todo) => ({
+    params: {
+      // ファイル名と合わせる ※文字列指定
+      id: todo.id.toString(),
+    },
+  }))
+  // paths：事前ビルドするパス対象を指定するパラメータ
+  // fallback：事前ビルドしたパス以外にアクセスしたときのパラメータ true:カスタム404Pageを表示 false:404pageを表示
+  return { paths, fallback: false }
+}
+
+// todosには上記pathsで指定した値が入る（1postずつ）
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  // 外部APIエンドポイントを呼び出しデータ取得
+  const res = await fetch(`http://localhost:8000/api/todos/${params.id}`)
+  const todo = await res.json()
+
+  // ページコンポーネントにpropsとしてに渡す
+  return {
+    props: {
+      todo,
+    },
+  }
+}
--- src/pages/todos/index.tsx
+++ src/pages/todos/index.tsx
@@ -3,13 +3,13 @@ import TodoList from 'components/TodoList'
 import { GetServerSideProps } from 'next'
 import { Todo } from 'components/Types'
 
-type ServerSideIndexProps = {
+type TodosServerSideIndexProps = {
   todos: Todo[]
 }
 
-const ServerSideIndex: React.FC<ServerSideIndexProps> = ({
+const Index: React.FC<TodosServerSideIndexProps> = ({
   todos,
-}: ServerSideIndexProps) => {
+}: TodosServerSideIndexProps) => {
   return (
     <>
       <h1>Hello, ToDoList👋</h1>
@@ -17,12 +17,12 @@ const ServerSideIndex: React.FC<ServerSideIndexProps> = ({
     </>
   )
 }
-export default ServerSideIndex
+export default Index
 
 //リクエストごとにデータフェッチ
 export const getServerSideProps: GetServerSideProps = async () => {
-  const response = await fetch('http://localhost:8000/api/todos')
-  const todos = await response.json()
+  const res = await fetch('http://localhost:8000/api/todos')
+  const todos = await res.json()
   //   pageディレクトリ配下のコンポーネントでは↓このようにreturn{ props: hoge }とするとhogeで参照できる
   return { props: { todos } }
 }
