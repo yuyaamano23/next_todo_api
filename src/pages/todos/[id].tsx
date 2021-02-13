d06a693ffbfbd2d6f280c889553788f0b46a3890
yuyaamano23 <nba.chako.nba@icloud.com>
2021-02-14 02:46:44 +0900

    edit処理できたー

--- src/pages/todos/[id].tsx
+++ src/pages/todos/[id].tsx
@@ -1,13 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Todo } from 'components/Types'
 import { GetStaticPaths, GetStaticProps } from 'next'
 import Link from 'next/link'
+import axios from 'axios'
+import Router from 'next/router'
 
 type TodoItemProps = {
   todo: Todo
 }
 
+type InputData = {
+  title: string
+  content: string
+}
+
 const TodoItemDetail: React.FC<TodoItemProps> = ({ todo }) => {
+  const [title, setTitle] = useState('')
+  const [content, setContent] = useState('')
+  const handleSubmit = (e) => {
+    // 現在のURLに対してフォームの送信が行われると、結果的にページがリロードされてしまいます。
+    // そのため、event.preventDefault()を呼び出し、buttonのデフォルトの動作をキャンセルするのよ。
+    e.preventDefault()
+
+    const data: InputData = {
+      title: title,
+      content: content,
+    }
+
+    axios({
+      method: 'put',
+      url: `http://localhost:8000/api/todos/${todo.id}`,
+      data: data,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(() => {
+        console.log('Edit success')
+        setTitle('')
+        setContent('')
+        // ページ更新させる
+        Router.push('/todos')
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
   return (
     <>
       <h1>ID: {todo.id}</h1>
@@ -17,6 +55,25 @@ const TodoItemDetail: React.FC<TodoItemProps> = ({ todo }) => {
       <Link href="/todos">
         <button>戻る</button>
       </Link>
+      <p>----------------------------------------</p>
+      <h1>自由に編集しろ</h1>
+      <p>title:</p>
+      <input
+        type="text"
+        placeholder={todo.title}
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <p>content:</p>
+      <input
+        type="text"
+        placeholder={todo.content}
+        value={content}
+        onChange={(e) => setContent(e.target.value)}
+      />
+      <div>
+        <button onClick={handleSubmit}>編集</button>
+      </div>
     </>
   )
 }
