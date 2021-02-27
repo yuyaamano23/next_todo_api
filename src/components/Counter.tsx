f594bb7eaadf33c46ebc002ea39d7b8e1b41f2ac
yuyaamano23 <nba.chako.nba@icloud.com>
2021-02-28 00:05:24 +0900

    dispatchしてstoreの更新がでけた

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -7,9 +7,23 @@ const Counter: React.FC = () => {
   const dispatch = useDispatch()
   const state = useCounterState().counter
 
+  const onClickIncrement = () => {
+    dispatch(counterSlice.actions.incrementCounter(1))
+  }
+
+  const onClickDecrement = () => {
+    dispatch(counterSlice.actions.decrementCounter(1))
+  }
+
   return (
     <>
       <div>{state.count}</div>
+      <button type="button" onClick={onClickIncrement}>
+        増やす
+      </button>
+      <button type="button" onClick={onClickDecrement}>
+        減らす
+      </button>
     </>
   )
 }
