f60cddd04ac0a6324783ed4d67b9749ad0834dd7
yuyaamano23 <nba.chako.nba@icloud.com>
2021-03-01 04:27:49 +0900

    とりあえずany型にすることでエラー解消

--- src/ducks/auth/selectors.ts
+++ src/ducks/auth/selectors.ts
@@ -1,8 +1,6 @@
 import { useSelector } from 'react-redux'
 import { AuthState } from './slice'
 
-// FIXME: なんかエラー出てる
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export const useAuthState = () => {
+export const useAuthState: any = () => {
   return useSelector((state: { auth: AuthState }) => state)
 }
