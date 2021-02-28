26deea4b1b1af32c7b3baadb9e9e591a7cb912f8
yuyaamano23 <nba.chako.nba@icloud.com>
2021-03-01 04:11:27 +0900

    tokenをヘッダーに含めてデータ取得成功

--- src/pages/mypage/index.tsx
+++ src/pages/mypage/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useDispatch } from 'react-redux'
 import authSlice from 'ducks/auth/slice'
 import { useAuthState } from 'ducks/auth/selectors'
+import axios from 'axios'
 
 const Index: React.FC = () => {
   const dispatch = useDispatch()
@@ -9,10 +10,37 @@ const Index: React.FC = () => {
 
   const onClickLoggedIn = () => {
     dispatch(authSlice.actions.loggedIn())
+    TokenCheck()
   }
 
   const onClickLoggedOut = () => {
     dispatch(authSlice.actions.loggedOut())
+  }
+
+  const TokenCheck = () => {
+    // localStorageから取得する予定
+    const email = 'naze@gmail.com'
+    const token = 'Nxyi1D0sgbllT28S6wAXN1AGhi4xVFDdGWJVm8qlruHX3KM97g2yTo3OEcUD'
+
+    axios({
+      method: 'get',
+      url: `${process.env.endPoint}/api/me`,
+      params: { email: email },
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((res) => {
+        if (res.data.id && res.data.name && res.data.email) {
+          console.log('token valid')
+        } else {
+          console.log('token invalid')
+        }
+      })
+      .catch((err) => {
+        console.log(err)
+      })
   }
 
   return (
