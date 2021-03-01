98bf7e63418cd3be2268028113527480cb16e7af
yuyaamano23 <nba.chako.nba@icloud.com>
2021-03-01 15:28:50 +0900

    email,tokenをlocalstrageに保存する関数を実装

---
+++ src/utils/tokenStorage.ts
@@ -0,0 +1,32 @@
+export const setItem: any = (key, value) => {
+  // localStorage以外の保存方法に切り替えるようにするため
+  // localStorage, sessionStorage, Cookie, ...
+  localStorage.setItem(key, value)
+}
+export const getItem: any = (key) => {
+  return localStorage.getItem(key)
+}
+const removeItem = (key) => {
+  localStorage.removeItem(key)
+}
+/**
+ * トークン・email保存する
+ *
+ * @param {String} token - 認証トークン
+ * @param {String} email - email
+ */
+export const saveAuthToken: any = (token, email) => {
+  setItem('token', token)
+  setItem('email', email)
+}
+/**
+ * 保存されたトークン・emailを削除する
+ */
+export const removeAuthTokens: any = () => {
+  removeItem('token')
+  removeItem('dmail')
+}
+
+export const getUserId: any = () => {
+  return getItem('email')
+}
