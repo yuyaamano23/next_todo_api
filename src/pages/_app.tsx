9a4258fb3cf057a97abe1294f18e37dfe2dfc152
yuyaamano23 <nba.chako.nba@icloud.com>
2021-03-01 16:30:26 +0900

    tokenが有効であるかの判定をapp.tsxに追加して、全ページ共通処理を書きました

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,11 @@
 import 'styles/globals.scss'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { AppProps } from 'next/app'
 import { Provider } from 'react-redux'
+import { useDispatch } from 'react-redux'
+import authSlice from 'ducks/auth/slice'
+import axios from 'axios'
+import { getItem } from 'utils/tokenStorage'
 import store from 'ducks/store'
 import Head from 'next/head'
 import { ThemeProvider } from '@material-ui/core/styles'
@@ -9,6 +13,40 @@ import CssBaseline from '@material-ui/core/CssBaseline'
 import theme from 'components/Theme'
 import NavBar from 'components/UIkit/NavBar'
 import Layout from 'components/Layout'
+
+// _app.tsxではprivederでラップしたコンポーネントしかstoreに接続できないので、AppInit関数を作ることで解決した
+function AppInit() {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    // クライアントのlocalstrogeにあるtokenが有効であるかどうかを全ページ共通の処理としてマウント時に1度だけ行う
+    const email = getItem('email')
+    const token = getItem('token')
+
+    axios({
+      method: 'get',
+      url: `${process.env.endPoint}/api/me`,
+      params: { email: email },
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((res) => {
+        if (res.data.id && res.data.name && res.data.email) {
+          console.log('token valid')
+          // storeのisLoggedInをtrueにする
+          dispatch(authSlice.actions.loggedIn())
+        } else {
+          console.log('token invalid')
+        }
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }, [])
+  return null
+}
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   React.useEffect(() => {
@@ -22,6 +60,7 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <Provider store={store}>
       <React.Fragment>
+        <AppInit />
         <Layout>
           <Head>
             <title>My page</title>
