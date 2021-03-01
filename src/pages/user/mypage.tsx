cff04bc5c1bc79c1098c878a02d0b73bc4728ef7
yuyaamano23 <nba.chako.nba@icloud.com>
2021-03-01 19:55:00 +0900

    ログイン中のユーザ情報をゲットして表示できたよ

--- src/pages/user/mypage.tsx
+++ src/pages/user/mypage.tsx
@@ -1,61 +1,61 @@
-import React from 'react'
-import { useDispatch } from 'react-redux'
-import authSlice from 'ducks/auth/slice'
+import React, { useEffect } from 'react'
+import { getUserEmail, getUserToken } from 'utils/tokenStorage'
 import { useAuthState } from 'ducks/auth/selectors'
-import { saveAuthToken } from 'utils/tokenStorage'
 import axios from 'axios'
 
+type userProfile = {
+  id: number | null
+  name: string | null
+  email: string | null
+  email_verified_at: string | null
+  api_token: string | null
+}
+
+// 初期値を定義しないとエラー
+const userNull = {
+  id: null,
+  name: 'ゲストユーザ',
+  email: null,
+  email_verified_at: null,
+  api_token: null,
+}
+
 const mypage: React.FC = () => {
-  const dispatch = useDispatch()
   const state = useAuthState().auth
+  const [userProfile, setUserProfile] = React.useState<userProfile>(userNull)
 
-  const onClickLoggedIn = () => {
-    dispatch(authSlice.actions.loggedIn())
-    TokenCheck()
-  }
-
-  const onClickLoggedOut = () => {
-    dispatch(authSlice.actions.loggedOut())
-  }
-
-  const TokenCheck = () => {
-    // localStorageから取得する予定
-    const email = 'naze@gmail.com'
-    const token = 'Nxyi1D0sgbllT28S6wAXN1AGhi4xVFDdGWJVm8qlruHX3KM97g2yTo3OEcUD'
-
+  useEffect(() => {
+    // 今後ログインしているユーザーのプロフィールを取得するapiを作りここで叩きたい
     axios({
       method: 'get',
       url: `${process.env.endPoint}/api/me`,
-      params: { email: email },
+      params: { email: `${getUserEmail()}` },
       headers: {
         'Content-Type': 'application/json',
-        Authorization: `Bearer ${token}`,
+        Authorization: `Bearer ${getUserToken()}`,
       },
     })
       .then((res) => {
-        if (res.data.id && res.data.name && res.data.email) {
-          console.log('token valid')
-          saveAuthToken()
-        } else {
-          console.log('token invalid')
-        }
+        setUserProfile(res.data)
       })
       .catch((err) => {
         console.log(err)
       })
-  }
+  }, [])
 
   return (
     <>
       {state.isLoggedIn ? (
         <>
           <div>ログイン中です</div>
-          <button onClick={onClickLoggedOut}>ログアウトする</button>
+          <div>{userProfile.id}</div>
+          <div>{userProfile.name}</div>
+          <div>{userProfile.email}</div>
         </>
       ) : (
         <>
           <div>ログアウト中です</div>
-          <button onClick={onClickLoggedIn}>ログインする</button>
+          <div>{userProfile.name}</div>
         </>
       )}
     </>
